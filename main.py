3b39b88354864e0afdc7d61e63ee1f574e9e885b
Adeyeye Gbenga <adeyeye546@gmail.com>
2021-02-19 20:03:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+check_height = float(height)
+check_weight = int(weight)
+
+BMI = check_weight/ check_height ** 2
+
+final_result = int(BMI)
+
+print(final_result)
+
+
+
+
+
+
+
+
+
